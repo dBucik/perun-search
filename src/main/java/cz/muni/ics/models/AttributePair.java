0cdb78fb305470523d95892546bb1b483fa28da7
Dominik František Bučík <bucik.dominik@gmail.com>
2018-07-01 13:19:18 +0200

    Added attribute as separated class

---
+++ src/main/java/cz/muni/ics/models/AttributePair.java
@@ -0,0 +1,28 @@
+package cz.muni.ics.models;
+
+public class AttributePair {
+
+    String attrName;
+    String attrValue;
+
+    public AttributePair(String attrName, String attrValue) {
+        this.attrName = attrName;
+        this.attrValue = attrValue;
+    }
+
+    public String getAttrName() {
+        return attrName;
+    }
+
+    public void setAttrName(String attrName) {
+        this.attrName = attrName;
+    }
+
+    public String getAttrValue() {
+        return attrValue;
+    }
+
+    public void setAttrValue(String attrValue) {
+        this.attrValue = attrValue;
+    }
+}
