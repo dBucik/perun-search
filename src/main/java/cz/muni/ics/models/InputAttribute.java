b195bac369e3a5abf8b160c23a86f00ae1dec0e4
Dominik František Bučík <bucik@ics.muni.cz>
2018-07-19 20:39:48 +0200

    Added type for InputAttribute
    Added type for Input Attribute. This way we can specify as what type we want the value to be treated.

--- src/main/java/cz/muni/ics/models/InputAttribute.java
+++ src/main/java/cz/muni/ics/models/InputAttribute.java
@@ -1,5 +1,7 @@
 package cz.muni.ics.models;
 
+import cz.muni.ics.models.attributes.InputAttributeType;
+
 /**
  * Attribute read from input for GraphQL.
  *
@@ -8,12 +10,16 @@ package cz.muni.ics.models;
 public class InputAttribute {
 
     private String key;
+    private String type;
+    private InputAttributeType type1;
     private String value;
 
     public InputAttribute() {}
 
-    public InputAttribute(String key, String value) {
+    public InputAttribute(String key, String type, String value) {
         this.key = key;
+        this.type = type;
+        this.type1 = resolveType(type);
         this.value = value;
     }
 
@@ -25,6 +31,24 @@ public class InputAttribute {
         this.key = key;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+        this.type1 = resolveType(type);
+    }
+
+    public InputAttributeType getType1() {
+        return type1;
+    }
+
+    public void setType1(String type) {
+        this.type = type;
+        this.type1 = resolveType(type);
+    }
+
     public String getValue() {
         return value;
     }
@@ -32,4 +56,24 @@ public class InputAttribute {
     public void setValue(String value) {
         this.value = value;
     }
+
+    private InputAttributeType resolveType(String type) {
+    	if (type == null || type.isEmpty()) {
+    		return InputAttributeType.STRING;
+		}
+
+        switch (type.toUpperCase()) {
+            case "STRING": return InputAttributeType.STRING;
+            case "EXACT_STRING": return InputAttributeType.EXACT_STRING;
+            case "INTEGER": return InputAttributeType.INTEGER;
+            case "BOOLEAN": return InputAttributeType.BOOLEAN;
+            case "ARRAY": return InputAttributeType.ARRAY;
+            case "MAP": return InputAttributeType.MAP;
+            case "LARGE_STRING": return InputAttributeType.LARGE_STRING;
+            case "EXACT_LARGE_STRING": return InputAttributeType.EXACT_LARGE_STRING;
+            case "LARGE_LIST": return InputAttributeType.LARGE_LIST;
+            default: return InputAttributeType.STRING;
+        }
+    }
+
 }
---
+++ src/main/java/cz/muni/ics/models/attributes/InputAttributeType.java
@@ -0,0 +1,15 @@
+package cz.muni.ics.models.attributes;
+
+public enum InputAttributeType {
+
+	STRING,
+	EXACT_STRING,
+	INTEGER,
+	BOOLEAN,
+	ARRAY,
+	MAP,
+	LARGE_STRING,
+	EXACT_LARGE_STRING,
+	LARGE_LIST
+
+}
